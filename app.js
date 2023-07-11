6a046dd65f0808ea346b58efd627ac844724d51e
Ajay Pawaskar <ajay.pawaskar@kloctechnologies.com>
2023-07-11 12:44:47 +0530

    db connection changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use('/movie',movieRoutes);
 
 
 
-const port = Number(process.env.PORT || 3306);
+const port = Number(process.env.PORT || 5000);
 const server = app.listen(port, () => {
 console.log(`Server is running on port ${port}.`);
 });
--- db/db.js
+++ db/db.js
@@ -1,22 +1,21 @@
 const mysql = require("mysql2");
 
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connection = mysql.createConnection(
-  // {
-  //   host: process.env.DB_HOST,
-  //   user: process.env.DB_USER,
-  //   password: process.env.DB_PASS,
-  //   database: process.env.DB_NAME,
-  //   port: process.env.PORT
-  // });
   {
-    host: 'sql.freedb.tech',      
-    user: 'freedb_movies-user',   
-    password: 'wNVp!*8r&EGG&?U',  
-    database: 'freedb_movies_d'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   });
+  // {
+  //   host: 'sql.freedb.tech',      
+  //   user: 'freedb_movies-user',   
+  //   password: 'wNVp!*8r&EGG&?U',  
+  //   database: 'freedb_movies_d'
+  // });
 
 
   connection.connect((err) => {
